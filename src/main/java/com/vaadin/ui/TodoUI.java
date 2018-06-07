f356c35558a58201b65a0ff60d232d60f15e2d4d
slava <slava_eclipse>
2018-06-07 18:13:25 +0600

    init

---
+++ src/main/java/com/vaadin/ui/TodoUI.java
@@ -0,0 +1,16 @@
+package com.vaadin.ui;
+
+import com.vaadin.server.VaadinRequest;
+
+
+public class TodoUI extends UI {
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+
+    }
+
+    @Override
+    public void markAsDirty() {
+
+    }
+}
